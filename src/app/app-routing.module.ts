326930a201ff701f0ea8b45516077dc1bc51d093
ashok <ashok.c.m9333@gmail.com>
2023-03-28 16:51:36 +0530

    coominting the files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RigistrationComponent } from './rigistration/rigistration.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {
+    path:"regiter",component:RigistrationComponent,
+  
+  },
+  {
+    path:"login",component:LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/common/header/header.component';
 import { FooterComponent } from './components/common/footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RigistrationComponent } from './rigistration/rigistration.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    RigistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
