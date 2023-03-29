a64f089acc2b785ca5d63024f7347f290e071b46
hussain <hussain.an.0404@gmail.com>
2023-03-29 10:28:52 +0530

    reslove conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { HomeComponent } from './components/home/home.component';
 import { CartComponent } from './components/cart/cart.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { RigistrationComponent } from './rigistration/rigistration.component';
 
 const routes: Routes = [
   {
@@ -18,22 +19,15 @@ const routes: Routes = [
   },
   {
     path:'cart',component:CartComponent
-=======
-import { LoginComponent } from './login/login.component';
-import { RigistrationComponent } from './rigistration/rigistration.component';
-
-
-const routes: Routes = [
-
+  },
   {
     path:"regiter",component:RigistrationComponent,
   
   },
   {
     path:"login",component:LoginComponent
->>>>>>> 326930a201ff701f0ea8b45516077dc1bc51d093
   }
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
